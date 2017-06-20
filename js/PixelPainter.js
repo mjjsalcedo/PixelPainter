7048785f4761ad19a0cf017788f5f68eff76ed9e
Mariel Salcedo <Mariel Salcedo>
2017-06-20 13:43:27 -1000

    added clear button

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -11,18 +11,19 @@ const PixelPainter = function(width, height, colors) {
   Palette.setAttribute('id', 'palette');
   Palette.innerHTML = createPalette(colors);
 
+  const clearButton = document.createElement("h2");
+  clearButton.setAttribute('id', 'clearButton');
+  clearButton.innerHTML = "Clear";
+
   ppCanvas.appendChild(Palette);
+
+  ppCanvas.appendChild(clearButton);
 
   pixelPainter.appendChild(ppCanvas);
 
   const Canvas = document.createElement('div');
   Canvas.setAttribute('id', 'canvas');
-  const canvas = createGrid(10,10, "<div class='cell'></div>");
 
-  var merged = [].concat.apply([], canvas).join('');
-
-  Canvas.innerHTML = merged;
-  ppCanvas.appendChild(Canvas);
 
   function createPalette(colors) {
     let palette = '';
@@ -34,7 +35,6 @@ const PixelPainter = function(width, height, colors) {
 
     return palette;
   }
-
 
 
   function createGrid(col, rows, fill) {
@@ -51,6 +51,31 @@ const PixelPainter = function(width, height, colors) {
   }
 
   let flag = false;
+
+  const domCanvas = renderCanvas(createGrid(10,10,"white"));
+  console.log(domCanvas);
+
+  Canvas.innerHTML = domCanvas;
+  ppCanvas.appendChild(Canvas);
+
+  function renderCanvas(grid){
+    let canvas = "";
+    console.log(grid);
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        canvas += "<div class='cell' style='background-color:" + grid[i][j] + ";'></div>";
+      }
+    }
+
+    return canvas;
+
+  }
+  clearButton.addEventListener("click", function(e){
+        domCanvas;
+
+  });
+
+
 
   Canvas.addEventListener("mousedown", function(e){
     if(e.target.className !== 'cell')
@@ -79,11 +104,11 @@ const PixelPainter = function(width, height, colors) {
       return;
 
     chosenColor = e.target.style.backgroundColor;
-  })
+  });
 
 };
+
 
 const colors = ['blue', 'black', 'red', 'yellow'];
 
 const pp = new PixelPainter(552, 500, colors);
-
